299f3edc4223fea6f21a53f12cd74bd4eb5438b6
Afif Ahmed <afif.ahmed@yahoo.com>
2026-06-26 21:05:05 +0600

    All models are completed

---
+++ models/parking_zone.go
@@ -0,0 +1,12 @@
+go
+package models
+import "time"
+type ParkingZone struct {
+    ID             uint `gorm:"primaryKey;autoIncrement" json:"id"`
+    Name           string `gorm:"not null" json:"name"`
+    Type           string `gorm:"not null" json:"type"` // "general", "ev_ch
+    TotalCapacity  int `gorm:"not null" json:"total_capacity"`
+    PricePerHour   float64 `gorm:"not null" json:"price_per_hour"`
+    CreatedAt      time.Time `json:"created_at"`
+    UpdatedAt      time.Time `json:"updated_at"`
+}
